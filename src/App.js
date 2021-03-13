5e896cfa3c473b4fc235f5a44ae889dcb4106482
Tiago <ottiagos@gmail.com>
2021-03-13 13:50:45 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import md5 from 'md5';
 
 function App() {
+
+  const [getData, setData] = useState({ comics: [], filtered: [] });
+  const { comics, filtered } = getData;
+
+  useEffect(() => {
+    console.log("Run");
+    const timeStamp = toString(new Date());
+    const privateKey = process.env.REACT_APP_PRIVATE_KEY;
+    const publicKey = process.env.REACT_APP_PUBLIC_KEY;
+    const hash = md5(timeStamp + privateKey + publicKey);
+
+    axios.get('http://gateway.marvel.com/v1/public/comics', {
+      params: {
+        'apikey': publicKey,
+        'limit': 100,
+        'hash': hash,
+        'ts': timeStamp
+      },
+      responseType: 'json'
+    }).then((response) => {
+
+      const results = response.data.data.results;
+      console.log(results);
+      setData({ comics: results, filtered: results });
+
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      Hello world
     </div>
   );
 }
